99e3f97df787f08326fb2f2263667449a5ff54ae
alenorgue <alenoriegag@gmail.com>
2025-05-22 16:25:29 +0200

    fixed, missin updated navbar

--- auth.js
+++ auth.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
